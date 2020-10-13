c31acc67307479389d82747f995befaf2fbef6bd
chupilko <chupilko.vika2015@gmail.com>
2020-10-13 09:48:25 +0300

    add logout and docs

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from accounts.views import CreateUserAPIView, LoginUserAPIView, ChangePasswordAPIView
+from accounts.views import CreateUserAPIView, LoginUserAPIView, ChangePasswordAPIView, LogoutUserAPIView
 
 urlpatterns = [
     path('register/', CreateUserAPIView.as_view(), name='register'),
     path('login/', LoginUserAPIView.as_view(), name='login'),
+    path('logout/', LogoutUserAPIView.as_view(), name='logout'),
     path('change_pass/', ChangePasswordAPIView.as_view(), name='change_pass')
 ]
